81eff8e10d5ac7eb0f8c158874b6db757e95bdc8
Agustin-Islas <agustinislas44@hotmail.com>
2023-10-21 19:00:26 -0300

    creada pagina de recepcion para clientes
    agregadas paginas de reintegro y cobertura

--- src/js/login.js
+++ src/js/login.js
@@ -2,7 +2,8 @@ function login(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(username == "cliente1" && password == "cliente1" || username == "cliente2" && password == "cliente2"){
-        window.location.href = "changePassword.html";
+        //window.location.href = "changePassword.html"; // @TODO descomentar
+        window.location.href = "recepcion_clientes.html";
     }else if(username == "empleado1" && password == "empleado1" || username == "empleado2" && password == "empleado2"){
         window.location.href = "registrarCliente.html";
     }else if(username == "admin" && password == "admin"){
