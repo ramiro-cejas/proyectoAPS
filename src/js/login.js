6a1c2392bdaa630b89f5be2119dccecdd1844872
Ramiro Cejas <84473095+ramiro-cejas@users.noreply.github.com>
2023-10-23 23:41:25 -0300

    Update - Solicitudes, Modificaciones, Listados

--- src/js/login.js
+++ src/js/login.js
@@ -1,9 +1,10 @@
 function login(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if(username == "cliente1" && password == "cliente1" || username == "cliente2" && password == "cliente2"){
-        //window.location.href = "changePassword.html"; // @TODO descomentar
+    if(username == "cliente1" && password == "cliente1"){   //cliente1 va a recepcion_clientes.html
         window.location.href = "recepcion_clientes.html";
+    }else if(username == "cliente2" && password == "cliente2"){ //cliente2 va a changePassword.html
+        window.location.href = "changePassword.html";
     }else if(username == "empleado1" && password == "empleado1" || username == "empleado2" && password == "empleado2"){
         window.location.href = "recepcion_empleado.html";
     }else if(username == "admin" && password == "admin"){
